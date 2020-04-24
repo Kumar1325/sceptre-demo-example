eb2bbee3e5882658320409cf3f51da31b0a89537
kumar <kumar@localhost.localdomain>
2020-04-24 23:02:38 +0530

    updated makefile with linting

--- makefile
+++ makefile
@@ -1,25 +1,42 @@
-.PHONY: deploy_dev deploy_prod destroy_dev destroy_prod validate_dev validate_prod
+.PHONY: deploy_dev deploy_prod destroy_dev destroy_prod validate_dev validate_prod test
+
+test:
+	@echo "Not test case written as of now, you can add them"
 
 validate_dev:
-  @echo "Validating DEV environment"
-  sceptre validate dev
+	@echo "Validating DEV environment"
+	@sceptre validate dev
   
 validate_prod:
-  @ehco "Validating PROD environment"
-  sceptre validate prod
+	@ehco "Validating PROD environment"
+	@sceptre validate prod
   
 deploy_dev: validate_dev
-  @echo "Deploying DEV environment"
-  @sceptre launch dev --yes
+	@echo "Deploying DEV environment"
+	@sceptre launch dev --yes
   
 deploy_prod: validate_prod
-  @echo "Deploying PROD environment"
-  @sceptre launch prod --yes
+	@echo "Deploying PROD environment"
+	@sceptre launch prod --yes
   
 destroy_dev:
-  @echo "Destroying DEV environment"
-  @sceptre delete dev --yes
+	@echo "Destroying DEV environment"
+	@sceptre delete dev --yes
   
 destroy_prod:
-  @echo "Destroying PROD environment"
-  @sceptre delete prod --yes
+	@echo "Destroying PROD environment"
+	@sceptre delete prod --yes
+
+.PHONY: linting
+linting: lint_pylint lint_flake8
+	@echo "Applied Linting with flake8 and pylint"
+
+.PHONY: lint_flake8
+lint_flake8:
+	@echo "Linting with Flake8"
+	@flake8 templates/ > flake8.log || :
+
+.PHONY: lint_pylint
+lint_pylint:
+	@echo "Linting with Pylint"
+	@pylint templates/ > pylint.log || :
--- templates/instance.py
+++ templates/instance.py
@@ -1,50 +1,49 @@
-from troposphere import Template, Ref, FindInMap, Output, Join
+from troposphere import Ref, Join
 from troposphere import Parameter, Tags
 import troposphere.ec2 as ec2
 from base import CloudformationAbstractBaseClass
 
 
 class Ec2InstanceClass(CloudformationAbstractBaseClass):
-	    
-		def __init__(self, sceptre_user_data):
-				super(self.__class__, self).__init__()
-				self.template.set_description("""Wordpress EC@""")
+    def __init__(self, sceptre_user_data):
+        super().__init__()
+        self.template.set_description("""Wordpress EC@""")
 
-				self.add_parameters()
-				self.add_resources()
+        self.add_parameters()
+        self.add_resources()
 
-		def add_parameters(self):
-				t = self.template
+    def add_parameters(self):
+        t = self.template
 
-				self.AmiId = t.add_parameter(
-					Parameter("AmiId", Description="Instnace ImageId", Type="String")
-				)
-				self.SubnetId = t.add_parameter(
-					Parameter("SubnetId", Description="VPC SubnetId", Type="String")
-				)
-				self.SecurityGroup = t.add_parameter(
-					Parameter(
-						"SecurityGroup", Description="Instance Security Group", Type="String"
-					)
-				)
+        self.AmiId = t.add_parameter(
+            Parameter("AmiId", Description="Instnace ImageId", Type="String")
+        )
+        self.SubnetId = t.add_parameter(
+            Parameter("SubnetId", Description="VPC SubnetId", Type="String")
+        )
+        self.SecurityGroup = t.add_parameter(
+            Parameter(
+                "SecurityGroup", Description="Instance Security Group", Type="String"
+            )
+        )
 
-		def add_resources(self):
-				self.Ec2Instance = self.template.add_resource(
-					ec2.Instance(
-						"Ec2Instance",
-						SubnetId=Ref(self.SubnetId),
-						InstanceType="t2.micro",
-						ImageId=Ref(self.AmiId),
-						SecurityGroupIds=[Ref(self.SecurityGroup)],
-						Tags=Tags(
-							Name=Join("", ["Ec2", Ref(self.Project)]),
-							Environment=Ref(self.Environment),
-							Project=Ref(self.Project),
-							Owner=Ref(self.Owner),
-							ExpirationDate=Ref(self.ExpirationDate)
-						)
-					)
-				)
+    def add_resources(self):
+        self.Ec2Instance = self.template.add_resource(
+            ec2.Instance(
+                "Ec2Instance",
+                SubnetId=Ref(self.SubnetId),
+                InstanceType="t2.micro",
+                ImageId=Ref(self.AmiId),
+                SecurityGroupIds=[Ref(self.SecurityGroup)],
+                Tags=Tags(
+                    Name=Join("", ["Ec2", Ref(self.Project)]),
+                    Environment=Ref(self.Environment),
+                    Project=Ref(self.Project),
+                    Owner=Ref(self.Owner),
+                    ExpirationDate=Ref(self.ExpirationDate),
+                ),
+            )
+        )
 
 
 def sceptre_handler(sceptre_user_data):
